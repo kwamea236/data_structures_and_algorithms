7804a00c20ec0824cb1fb638d4d4f68d5a32287f
kwamea236 <kwamea236@protonmail.com>
2023-06-28 00:49:31 +0000

    bubblesort algorithm code

--- src/index.js
+++ src/index.js
@@ -13,3 +13,40 @@ function linearsearch(arr, num){
 
     return null;
 }
+
+/**
+ * 
+ * @param {number[]} arr 
+ * @param {number} a 
+ * @param {number} b 
+ */
+
+function swap(arr, a, b){
+    let temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
+
+
+
+/**
+ * this function is a buble sort algorithms
+ * 
+ * @param {number[]} arr 
+ */
+function bubblesort(arr){
+    let len = arr.length;
+
+    for(let i=0; i< len; i++){
+        for(let j=0; j < len -1; j++){
+            if(arr[j] > arr[j + 1]){
+                swap(arr, j, j+1);
+            }
+        }
+    }
+
+    return arr;
+}
+
+
+console.log(bubblesort([9,1,4,6,3,2,5]));
